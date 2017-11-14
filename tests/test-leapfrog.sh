a4ba9f876de6b92d0923503bc68692587437dd94
Antony Messerli <amesserl@rackspace.com>
2017-11-14 11:13:15 -0600

    Initial MNAIO deploy test

--- tests/test-leapfrog.sh
+++ tests/test-leapfrog.sh
@@ -16,6 +16,8 @@
 
 export VALIDATE_UPGRADE_INPUT=false
 export AUTOMATIC_VAR_MIGRATE_FLAG="--for-testing-take-new-vars-only"
+export MNAIO_SSH="ssh -oStrictHostKeyChecking=no root@infra1"
+export RE_JOB_IMAGE_TYPE="${RE_JOB_IMAGE_TYPE:-aio}"
 
 if [ "${RE_JOB_SERIES}" == "kilo" ]; then
   export RPC_TARGET_CHECKOUT="r14.2.0"
@@ -28,11 +30,43 @@ elif [ "${RE_JOB_SERIES}" == "mitaka" ]; then
   export OA_OPS_REPO_BRANCH="0690bb608527b90596e5522cc852ffa655228807"
 fi
 
-sudo --preserve-env $(readlink -e $(dirname ${0}))/../scripts/ubuntu14-leapfrog.sh
+function aio_leap {
+  sudo --preserve-env $(readlink -e $(dirname ${0}))/../scripts/ubuntu14-leapfrog.sh
 
+  # if rpc-maas repo exists, run maas-verify
+  if [ -d "/opt/rpc-maas" ]; then
+    pushd /opt/rpc-maas/playbooks
+      openstack-ansible maas-verify.yml -vv
+    popd
+  fi
+}
+
+function mnaio_leap {
+  scp -r -o StrictHostKeyChecking=no /opt/rpc-upgrades infra1:/opt/
+
+  # start the rpc-o leapfrog upgrade from infra1
+  ${MNAIO_SSH} "pushd /opt/rpc-upgrades ; \
+                export TERM=linux ; \
+                export RPC_TARGET_CHECKOUT=${RPC_TARGET_CHECKOUT}; \
+                export OA_OPS_REPO_BRANCH=${OA_OPS_REPO_BRANCH}; \
+                export VALIDATE_UPGRADE_INPUT=false; \
+                export AUTOMATIC_VAR_MIGRATE_FLAG=${AUTOMATIC_VAR_MIGRATE_FLAG}; \
+                export ANSIBLE_FORCE_COLOR=true; \
+                echo 'neutron_legacy_ha_tool_enabled: true' >> /etc/openstack_deploy/user_variables.yml; \
+                ./scripts/ubuntu14-leapfrog.sh"
+  ssh -T -o StrictHostKeyChecking=no infra1 << 'EOF'
+set -xe
 # if rpc-maas repo exists, run maas-verify
 if [ -d "/opt/rpc-maas" ]; then
-  pushd /opt/rpc-upgrades/playbooks
+  pushd /opt/rpc-maas/playbooks
     openstack-ansible maas-verify.yml -vv
   popd
+fi
+EOF
+}
+
+if [ "${RE_JOB_IMAGE_TYPE}" = "mnaio" ]; then
+  mnaio_leap
+else
+  aio_leap
 fi
