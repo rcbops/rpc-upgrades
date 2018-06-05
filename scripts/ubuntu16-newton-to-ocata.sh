3cf351c6dc0b4aec1171bf742a3981f374fab11b
Antony Messerli <amesserl@rackspace.com>
2018-06-04 20:02:38 -0500

    RLM-1471 Adds playbook for prepping incrementals
    Adds playbook for prepping incremental upgrades, fixes up
    repos, brings all packages to latest, fixes up configs before
    running upgrades.

--- scripts/ubuntu16-newton-to-ocata.sh
+++ scripts/ubuntu16-newton-to-ocata.sh
@@ -36,40 +36,17 @@ else
   popd
 fi
 
-##### rpc-o newton to ocata openstack-ansible transition mods
-
-touch /etc/openstack_deploy/user_secrets.yml
-rm /etc/openstack_deploy/user_secrets.yml
-ln -s /etc/openstack_deploy/user_osa_secrets.yml /etc/openstack_deploy/user_secrets.yml
-if ! grep -i "ironic_galera_password" /etc/openstack_deploy/user_osa_secrets.yml; then
-  echo "ironic_galera_password: blah" >> /etc/openstack_deploy/user_osa_secrets.yml
-fi
-
-# remove variables that will cause issues jumping to ocata
-sed -i '/^openstack_release.*/d' /etc/openstack_deploy/user_osa_variables_defaults.yml
-sed -i '/^neutron_lbaas_git_repo.*/d' /etc/openstack_deploy/user_osa_variables_defaults.yml
-sed -i '/^neutron_lbaas_git_install_branch.*/d' /etc/openstack_deploy/user_osa_variables_defaults.yml
-sed -i '/^# Use rcbops version of neutron-lbaas.*/d' /etc/openstack_deploy/user_osa_variables_defaults.yml
-
-# 10.1 client doesn't exist on rackspace mirrors
-#sed -i 's/^galera_client_apt_repo_url:.*/galera_client_apt_repo_url: http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu/g' /etc/openstack_deploy/user_rpco_galera.yml
-#sed -i 's/^galera_apt_repo_url:.*/galera_apt_repo_url: http://ftp.utexas.edu/mariadb/repo/10.1/ubuntu/g' /etc/openstack_deploy/user_rpco_galera.yml
-touch /etc/openstack_deploy/user_rpco_galera.yml
-rm /etc/openstack_deploy/user_rpco_galera.yml
-
-# remove artifacts repo, NOTE: will need to run on all hosts on MNAIO
-if [ -f "/etc/apt/sources.list.d/rpco.list" ]; then
-  rm -f /etc/apt/sources.list.d/rpco.list
-fi
-
-# ensure we have the latest Xenial packages before we start any upgrades
-apt-get -qq update; DEBIAN_FRONTEND=noninteractive apt-get -y dist-upgrade
-
-##### end rpc-o to openstack-ansible transition mods
-
 pushd /opt/openstack-ansible
   git checkout ${OSA_SHA}
   scripts/bootstrap-ansible.sh
+popd
+
+# configure apt sources and update all Xenial Packages before jump to Ocata
+pushd /opt/rpc-upgrades/playbooks
+  openstack-ansible prepare-ocata-upgrade.yml
+popd
+
+pushd /opt/openstack-ansible
   source /usr/local/bin/openstack-ansible.rc
   export TERM=linux
   export I_REALLY_KNOW_WHAT_I_AM_DOING=true
