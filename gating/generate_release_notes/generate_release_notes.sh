1482be8da1c762227f8cb240591fe287028274cf
rexredinger <alex.redinger@rackspace.com>
2018-08-02 16:40:20 -0400

    Ensure failed release notes jobs return nonzero exit code

--- gating/generate_release_notes/generate_release_notes.sh
+++ gating/generate_release_notes/generate_release_notes.sh
@@ -1,6 +1,14 @@
-#!/bin/bash -xe
+#!/bin/bash -x
 
 # This script is run within a docker container to generate release notes.
 
 /generate_reno_report.sh $NEW_TAG reno_report.md
-cat reno_report.md > all_notes.md
+report_status=$?
+
+if [[ ${report_status} == 0 ]]; then
+	cat reno_report.md > all_notes.md
+else
+	cat reno_report.md.err
+fi
+
+exit ${report_status}
