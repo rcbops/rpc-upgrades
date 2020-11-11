0b7cad9e36691a9ca72fb8135f5a3d2b97a7e95d
Bjoern Teipel <bjoern.teipel@rackspace.com>
2020-11-11 17:41:34 -0600

    Adding stub upgrade scripts for ussuri

---
+++ incremental/ubuntu18-upgrade-to-ussuri.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright 2019, Rackspace US, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -evu
+
+source lib/functions.sh
+source lib/vars.sh
+
+require_ubuntu_version 18
+
+export OSA_SHA="21.1.0"
+export SKIP_INSTALL=${SKIP_INSTALL:-'no'}
+export RPC_PRODUCT_RELEASE="ussuri"
+export RPC_ANSIBLE_VERSION="2.9.9"
+
+check_rpc_config
+mark_started
+checkout_openstack_ansible
+ensure_osa_bootstrap
+prepare_ussuri
+run_upgrade
+mark_completed
