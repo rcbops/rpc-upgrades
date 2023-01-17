fd4d442b74f111855cb2604de00e1288aa3df424
Bjoern Teipel <bjoern.teipel@rackspace.com>
2023-01-17 12:51:29 -0600

    Adding stub code for wallaby upgrade

--- incremental/lib/functions.sh
+++ incremental/lib/functions.sh
@@ -80,6 +80,10 @@ function determine_release {
       export CODE_UPGRADE_FROM="victoria"
       echo "You seem to be running Victoria"
     ;;
+    *23|wallaby)
+      export CODE_UPGRADE_FROM="wallaby"
+      echo "You seem to be running Wallaby"
+    ;;
     *)
       echo "Unable to detect current OpenStack version, failing...."
       exit 99
@@ -369,6 +373,18 @@ function prepare_victoria {
     openstack-ansible mariadb-slaves-shutdown.yml
     #if [[ ! -f "${UPGRADES_WORKING_DIR}/victoria_upgrade_prep.complete" ]]; then
     #  openstack-ansible prepare-victoria-upgrade.yml
+    #fi
+  popd
+}
+
+function prepare_wallaby {
+  ensure_working_dir
+
+  pushd /opt/rpc-upgrades/incremental/playbooks
+    # Shutdown mariadb slaves to ease version upgrades
+    openstack-ansible mariadb-slaves-shutdown.yml
+    #if [[ ! -f "${UPGRADES_WORKING_DIR}/wallaby_upgrade_prep.complete" ]]; then
+    #  openstack-ansible prepare-wallaby-upgrade.yml
     #fi
   popd
 }
